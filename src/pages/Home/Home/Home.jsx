1b499d7bb6e3b22ac0ce8d48e31e93cd21de29e6
tahminatanjidamiti <mity2027@gmail.com>
2025-01-19 15:56:03 +0600

    top scholarships added

--- src/pages/Home/Home/Home.jsx
+++ src/pages/Home/Home/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Banner from '../Banner/Banner';
+import TopScholarship from '../TopScholarship/TopScholarship';
 
 const Home = () => {
     return (
         <div>
             <Banner></Banner>
+            <TopScholarship></TopScholarship>
         </div>
     );
 };
---
+++ src/pages/ScholarshipDetails/ScholarshipDetails.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ScholarshipDetails = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default ScholarshipDetails;
